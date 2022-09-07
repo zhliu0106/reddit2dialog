c8002a82a664b92940130cae12c7529d81234b54
zhliu0106 <zhliu0106@gmail.com>
2022-09-07 12:05:16 +0800

    remove emojis

--- utils.py
+++ utils.py
@@ -13,6 +13,29 @@ with open("filtered_reddits.txt") as f:
         FILTERED_REDDITS.extend(line.strip().split())
 
 URL_REGEX = re.compile(f"https?://(?:[-\w.]|(?:%[\da-fA-F]{2}))+")
+EMOJIS_REGEX = re.compile(
+    "["
+    "\U0001F600-\U0001F64F"  # emoticons
+    "\U0001F300-\U0001F5FF"  # symbols & pictographs
+    "\U0001F680-\U0001F6FF"  # transport & map symbols
+    "\U0001F1E0-\U0001F1FF"  # flags (iOS)
+    "\U00002500-\U00002BEF"  # chinese char
+    "\U00002702-\U000027B0"
+    "\U00002702-\U000027B0"
+    "\U000024C2-\U0001F251"
+    "\U0001f926-\U0001f937"
+    "\U00010000-\U0010ffff"
+    "\u2640-\u2642"
+    "\u2600-\u2B55"
+    "\u200d"
+    "\u23cf"
+    "\u23e9"
+    "\u231a"
+    "\ufe0f"  # dingbats
+    "\u3030"
+    "]+",
+    re.UNICODE,
+)
 HTML_PAIRS = [
     ("&amp;", " & "),
     ("&quot", ' " '),
@@ -21,6 +44,7 @@ HTML_PAIRS = [
     ("&lt;", " < "),
 ]
 
+
 class Comment(msgspec.Struct):
     body: str
     id: str
@@ -28,6 +52,7 @@ class Comment(msgspec.Struct):
     parent_id: str
     subreddit: str
     author: str
+
 
 class TimeoutException(Exception):
     pass
@@ -51,6 +76,7 @@ def preprocess(string):
         string = string.replace(a, b)
     for a in ["\n", "\r", "<", ">", "``", "''", "*"]:
         string = string.replace(a, " ")
+    string = re.sub(EMOJIS_REGEX, '', string)
     res = " ".join(string.strip().split())
     return res
 
